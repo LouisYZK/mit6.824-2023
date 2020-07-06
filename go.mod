83b790d162054228f6836cf3f5b77c588a131c7f
LouisYZK <yangzhikai@cug.edu.cn>
2020-07-06 21:39:23 +0800

    make go work

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/k0kubun/pp v3.0.1+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518
 )
--- pydemo/test.py
+++ pydemo/test.py
@@ -65,4 +65,134 @@ async def main():
         asyncio.create_task(coroutine_demo(i))
     # await gather(*tasks)
 
-ioloop.run_until_complete(main())
+# ioloop.run_until_complete(main())
+
+from multiprocessing import Queue
+from threading import Thread
+import threading
+con = threading.Condition()
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+
+    def foo(self) -> None:
+        con.acquire()
+        # for i in range(self.n):
+            
+        #     # printFoo() outputs "foo". Do not change or remove this line.
+        #     print('foo')
+        #     con.notify()
+        #     con.wait()
+        print('hi!')
+        con.notify()
+        con.wait()
+        print('end!')
+        con.release()
+
+
+    def bar(self) -> None:
+        con.acquire()
+            # for i in range(self.n):
+            #     # printBar() outputs "bar". Do not change or remove this line.
+            #     con.wait()
+            #     print('bar')
+            #     con.notify()
+        con.wait()
+        print('i see...')
+        con.notify()
+        con.release()
+
+import time
+def foo() -> None:
+    con.acquire()
+    print(con)
+    # for i in range(self.n):
+        
+    #     # printFoo() outputs "foo". Do not change or remove this line.
+    #     print('foo')
+    #     con.notify()
+    #     con.wait()
+    print('hi!')
+    con.notify()
+    con.wait()
+    print('end!')
+    con.notify()
+    con.release()
+
+
+def bar() -> None:
+    con.acquire()
+    print(con)
+        # for i in range(self.n):
+        #     # printBar() outputs "bar". Do not change or remove this line.
+        #     con.wait()
+        #     print('bar')
+        #     con.notify()
+    con.wait()
+    print('i see...')
+    con.notify()
+    con.wait()
+    print('i see...')
+    con.notify()
+    con.release()
+
+# fb = FooBar(2)
+# t1 = Thread(target=foo)
+# t2 = Thread(target=bar)
+# t1.start()
+# t2.start()
+# t1.join()
+# t2.join()
+
+import threading
+ 
+
+from multiprocessing import Queue
+q = Queue()
+qe = Queue()
+qo = Queue()
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+        q.put(0)
+        
+	# printNumber(x) outputs "x", where x is an integer.
+    def zero(self, printNumber: 'Callable[[int], None]') -> None:
+        i = 0
+        while True:
+            num = q.get()
+            printNumber(num)
+            i += 1
+            if i % 2 == 0:
+                qe.put(i)
+            else:
+                qo.put(i)
+            if i == self.n:
+                qe.put(0)
+                qo.put(0)
+                break
+        
+    def even(self, printNumber: 'Callable[[int], None]') -> None:
+        while True:
+            num = qe.get()
+            if not num: break
+            printNumber(num)
+            q.put(0)
+        
+    def odd(self, printNumber: 'Callable[[int], None]') -> None:
+        while True:
+            num = qo.get()
+            if not num: break
+            printNumber(num)
+            q.put(0)
+ 
+if __name__ == "__main__":
+ 
+    def printNumber(n):
+        print(n)
+    
+    so = ZeroEvenOdd(5)
+    t1 = Thread(target=so.zero, args=(printNumber,))
+    t2 = Thread(target=so.odd, args=(printNumber,))
+    t3 = Thread(target=so.even, args=(printNumber,))
+    t2.start(); t3.start(); t1.start()
