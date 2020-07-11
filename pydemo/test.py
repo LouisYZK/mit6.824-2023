9c8f8803000669a2fd7cce4265e5c0821ac96b70
LouisYZK <yangzhikai@cug.edu.cn>
2020-07-11 22:23:01 +0800

    udpaye

--- pydemo/test.py
+++ pydemo/test.py
@@ -185,14 +185,132 @@ class ZeroEvenOdd:
             if not num: break
             printNumber(num)
             q.put(0)
- 
+class AllOne:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.kv = {}
+        self.min_stack = []
+        self.max_stack = []
+
+
+    def inc(self, key: str) -> None:
+        """
+        Inserts a new key <Key> with value 1. Or increments an existing key by 1.
+        """
+        if key not in self.kv:
+            self.kv[key] = 1
+        else:
+            self.kv[key] += 1
+        val = self.kv[key]
+
+        if not self.max_stack:
+            self.max_stack.append((key, val))
+        else:
+            c_key, c_max = self.max_stack[-1]
+            if c_key == key:
+                self.max_stack.pop()
+                self.max_stack.append((key, val))
+                print('min: ', self.min_stack)
+            elif c_max <= val:
+                self.max_stack.append((key, val))
+        
+        if not self.min_stack:
+            self.min_stack.append((key, val))
+        else:
+            c_key, c_min = self.min_stack[-1]
+            if c_key == key:
+                self.min_stack.pop()
+                _, n_min = self.min_stack[-1]
+                if n_min >= val:
+                    self.min_stack.append((key, val))
+                print('min: ', self.min_stack)
+            elif c_min >= val:
+                self.min_stack.append((key, val))
+        print('min: ', self.min_stack)
+
+
+    def dec(self, key: str) -> None:
+        """
+        Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
+        """
+        if key not in self.kv:
+            return
+        val = self.kv[key]
+        if val == 1:
+            self.kv.pop(key)
+            k_max, v_max = self.max_stack[-1]
+            k_min, v_min = self.min_stack[-1]
+            if k_max == key:
+                self.max_stack.pop()
+            if k_min == key:
+                self.min_stack.pop()
+        else:
+            self.kv[key] -= 1
+            cv = self.kv[key]
+            k_max, v_max = self.max_stack[-1]
+            k_min, v_min = self.min_stack[-1]
+            if k_max == key:
+                self.max_stack.pop()
+                c_max_key, c_max_val = self.max_stack[-1]
+                if c_max_val <= cv:
+                    self.max_stack.append((key, cv))
+                print('min: ', self.min_stack)
+            elif k_min == key:
+                self.min_stack.pop()
+                self.min_stack.append((key, cv))
+                print('min: in dec ', self.min_stack)
+            
+            if v_max <= cv:
+                self.max_stack.append((key, cv))
+            if v_min >= cv:
+                self.min_stack.append((key, cv))
+        print('min: ', self.min_stack)
+            
+
+    def getMaxKey(self) -> str:
+        """
+        Returns one of the keys with maximal value.
+        """
+        if not self.max_stack:
+            return ""
+        k, v = self.max_stack[-1]
+        return k
+
+
+    def getMinKey(self) -> str:
+        """
+        Returns one of the keys with Minimal value.
+        """
+        if not self.min_stack:
+            return ""
+        k,v = self.min_stack[-1]
+        return k
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
 if __name__ == "__main__":
  
     def printNumber(n):
         print(n)
     
-    so = ZeroEvenOdd(5)
-    t1 = Thread(target=so.zero, args=(printNumber,))
-    t2 = Thread(target=so.odd, args=(printNumber,))
-    t3 = Thread(target=so.even, args=(printNumber,))
-    t2.start(); t3.start(); t1.start()
+    # so = ZeroEvenOdd(5)
+    # t1 = Thread(target=so.zero, args=(printNumber,))
+    # t2 = Thread(target=so.odd, args=(printNumber,))
+    # t3 = Thread(target=so.even, args=(printNumber,))
+    # t2.start(); t3.start(); t1.start()
+
+    obj = AllOne()
+    obj.inc('a')
+    obj.inc('b'); obj.inc('b'); obj.inc('b'); obj.inc('b')
+    obj.dec('b'); obj.dec('b')
+    print(obj.getMaxKey())
+    print(obj.getMinKey())
+    print(obj.kv, obj.max_stack, obj.min_stack)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# MIT 6.824 Lab 2020
+This repo repr some demo programs about mit6.824 distributed systems lab only for studying.
+
+Besides golang used in lecture, i also use python which i am more familiar with to implemmnet the lab. `goroutine` and python `thread` 's synchornizing are same to some degree.
+
+If you find some errors, welcome to issue me !~
+
+- [https://github.com/LouisYZK/mit6.824-2020/tree/master/pydemo/mr_with_crash](Lab1-MapReduce golang version)
+- [https://github.com/LouisYZK/mit6.824-2020/tree/master/src/mr](Lab1-MapReuduce python version)
+
+continue to finish...
+
+- [](lab-2)
